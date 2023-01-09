8eb367124530e8496023b8c005ecf59c7b3221ee
Arlo Fonseca <thedondada7801@gmail.com>
2023-01-09 09:42:36 -0500

    chore(manifest): bump version

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,40 +1,39 @@
--- FX Information
+--[[ FX Information ]]--
 fx_version "cerulean"
-lua54 "yes"
 game "gta5"
+lua54 "yes"
 
--- Resource Information
-name "qhospital"
+--[[ Resource Information ]]--
+name "qpr_hospital"
+version "0.0.6"
+description "Hospital system for overextended framework"
 author "Qpr"
-repository "https://github.com/arlofonseca/qhospital"
-description ''
+repository "https://github.com/arlofonseca/qpr_hospital"
 
--- Manifest
+--[[ Manifest ]]--
+dependencies {
+	"/onesync",
+	"ox_core",
+	"ox_lib",
+	"ox_inventory",
+}
+
 shared_scripts {
 	"@ox_lib/init.lua",
 	"modules/init.lua",
 }
 
 client_scripts {
-	"client/class/sync.lua",
 	"modules/data.lua",
-	"client/main.lua",
+	"client/*.lua",
 }
 
 server_scripts {
-	'@oxmysql/lib/MySQL.lua',
+	"@oxmysql/lib/MySQL.lua",
 	"@ox_core/imports/server.lua",
-	"server/class/player.lua",
-	"server/main.lua",
+	"server/*.lua",
 }
 
 files {
 	"modules/config.json",
-}
-
-dependencies {
-	"ox_core",
-	"oxmysql",
-	"ox_lib",
-	"ox_inventory",
 }
